3f467b3916555dd0fa91595658f84a9031b6be70
temmytifs <temmytifs@gmail.com>
2023-06-15 13:18:03 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <studio.h>
+/** 
+ *main -Entry point 
+ *
+ *Return Always 0 (Sucess) 
+ */ 
+int main(void) 
+{
+         put("\" Programming is like building a multilingual puzzel 
+'); 
+         retun(0)
+} 
