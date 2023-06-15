3df6cdf00b812b12915831694e47b0112a317d78
temmytifs <temmytifs@gmail.com>
2023-06-15 14:09:37 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
-/** 
- *main -Entry point 
+/**
+ * main - Entry point
  *
- *Return Always 0 (Sucess) 
- */ 
-int main(void) 
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
-         put("\" Programming is like building a multilingual puzzel 
-'); 
-         retun(0)
-} 
+ put("\"Programming is like building a multilingual puzzle
+;)
+    return (0);
+}
