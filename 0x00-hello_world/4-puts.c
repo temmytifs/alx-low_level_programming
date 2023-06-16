8fd22602d887027388e98cb929331653c69713d5
temmytifs <temmytifs@gmail.com>
2023-06-16 01:18:53 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,10 @@
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)
+ * Return: Always 0 (Sucess)
  */
 int main(void)
 {
- put("\"Programming is like building a multilingual puzzle;)
-    return (0);
+	puts("Programming is like building a multilingual puzzle\n");
+	retun (0);
 }
