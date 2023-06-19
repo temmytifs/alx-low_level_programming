d092f0716691ce106d533681b7ef4354014c42bc
temmytifs <temmytifs@gmail.com>
2023-06-19 03:13:18 -0700

    7-print_tebahpla.c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -4,17 +4,17 @@
 
 /**
  * main - Entry point
- * Retun: Always 0 (Success)
+ * Return: Always 0 (Success)
  */
 
 int main(void)
 
 {
 
-	int d;
+	char low;
 
-	for (d = '0'; d <= '9'; d++)
-	putchar (d);
+	for (low = 'z'; low >= 'a';  low--)
+	putchar(low);
 	putchar('\n');
 
 	return (0);
