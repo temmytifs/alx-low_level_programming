a6c7270e5cdcd261c671bd9e2c73840554da265f
temmytifs <temmytifs@gmail.com>
2023-06-23 20:29:21 -0700

    2main.c

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -14,7 +14,7 @@ int largest;
 
 if (a > b && b > c)
 {
-largest = a;
+largest = b;
 }
 else if (b > a && a > c)
 {
@@ -22,7 +22,7 @@ largest = b;
 }
 else
 {
-largest = c;
+largest = a;
 }
 
 return (largest);
