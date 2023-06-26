920252d3cc0632153b592b16caafda7b94a9915c
temmytifs <temmytifs@gmail.com>
2023-06-26 06:32:41 -0700

    redo 2

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,8 +2,8 @@
 
 /**
  * _strlen - Retruns the length of a string.
- * @str: The string to get the length of 
- * Retrun: The length of @str.
+ * @str: The string to get the length of
+ * Return: The length of @str.
  */
 size_t _strlen(const char *str)
 
