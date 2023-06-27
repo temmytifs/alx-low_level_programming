1fbd23566c1d692d0b71d2f328fe038cb0a138c9
temmytifs <temmytifs@gmail.com>
2023-06-26 18:13:29 -0700

    1-swap

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -3,7 +3,7 @@
 /**
  * swap_int - Swaps the values of two integers.
  * @a: The first integer to be swapped.
- * @b The second integer to be swapped.
+ * @b: The second integer to be swapped.
  *
  * Return: nothing
  */
