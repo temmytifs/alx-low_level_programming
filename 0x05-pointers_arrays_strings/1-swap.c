9962e090c32b5e5944ab4779520b45fa00769057
temmytifs <temmytifs@gmail.com>
2023-06-26 17:51:00 -0700

    new

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,9 +1,2 @@
-#include <stdio.h>
 
-/**
- * swap_int - Swaps the values of tow integers.
- * @a: The first integer to be swapped.
- * @b: The second integer to be swwapped. 
- *
- * RETURN: :DNOTHING 
  
