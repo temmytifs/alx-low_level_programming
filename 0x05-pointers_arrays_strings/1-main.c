5d6769812eca18c142af7cf54ab7b7c3487677a4
temmytifs <temmytifs@gmail.com>
2023-06-26 18:06:50 -0700

    1-swap

--- 0x05-pointers_arrays_strings/1-main.c
+++ 0x05-pointers_arrays_strings/1-main.c
@@ -14,8 +14,8 @@ int main(void)
 
 	a = 98;
 	b = 42;
-	printf("a=%d, b=%d\n", a, b)
+	printf("a=%d, b=%d\n", a, b);
 	swap_int(&a, &b);
-	printf("a=%d, b=%d\n", a, b)
+	printf("a=%d, b=%d\n", a, b);
 	return (0);
 }
