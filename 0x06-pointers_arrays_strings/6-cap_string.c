6b526b6117ce9d488f751eb181202814d332b880
temmytifs <temmytifs@gmail.com>
2023-06-29 18:51:39 -0700

    6-cap

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,36 +1,41 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * cap_string - Capitalizes all words of a string.
- * @str: The string to be captalized.
- * Return: A pointer to the changed string.
+ * cap_string - capitalizes most of the words in a string.
+ * @s: analized string.
+ *
+ * Return: String with all words capitalized.
  */
 
-char *cap_string(char *str)
+char *cap_string(char *s)
 
 {
-	int index = 0;
 
-	while (str[index])
+	int i, j;
+	int a[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
+
+	i = 0;
+	while (*(s + i) != '\0')
 	{
-	while (!(str[index] >= 'a' && str[index] <= 'z'))
-	index++;
-	if (str[index - 1] == ' ' ||
-	str[index - 1] == '\t' ||
-	str[index - 1] == '\n' ||
-	str[index - 1] == ',' ||
-	str[index - 1] == ';' ||
-	str[index - 1] == '.' ||
-	str[index - 1] == '!' ||
-	str[index - 1] == '?' ||
-	str[index - 1] == '"' ||
-	str[index - 1] == '(' ||
-	str[index - 1] == ')' ||
-	str[index - 1] == '{' ||
-	str[index - 1] == '}' ||
-	index == 0)
-	str[index] -= 32;
-	index++;
+	if (*(s + i) >= 'a' && *(s + i) <= 'z')
+	{
+	if (i == 0)
+	{
+	*(s + i) = *(s + i) - 32;
 	}
-	return (str);
+	else
+	{
+	for (j = 0; j <= 12; j++)
+	{
+	if (a[j] == *(s + i - 1))
+	{
+	*(s + i) = *(s + i) - 32;
+	}
+	}
+	}
+	}
+	i++;
+	}
+	return (s);
 }
