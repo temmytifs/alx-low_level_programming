7fc62676750ff93c104de6719096aeb73eb9176f
temmytifs <temmytifs@gmail.com>
2023-07-27 07:17:13 -0700

    0x012.c

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -7,15 +7,15 @@
  *
  * Return: number of elements in h
  */
+
 size_t list_len(const list_t *h)
 {
- size_t n = 0;
+	size_t n = 0;
 
- while (h)
- {
- n++;
- h = h->next;
- }
- return (n);
+	while (h)
+	{
+	n++;
+	h = h->next;
+	}
+	return (n);
 }
-
--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -9,23 +9,25 @@
  *
  * Return: the address of the new element, or NULL if it fails
  */
+
 list_t *add_node(list_t **head, const char *str)
 {
- list_t *new;
- unsigned int len = 0;
+	list_t *new;
+	unsigned int len = 0;
 
- while (str[len])
- len++;
+	while (str[len])
+	len++;
+	new = malloc(sizeof(list_t));
 
- new = malloc(sizeof(list_t));
- if (!new)
- return (NULL);
+	if (!new)
 
- new->str = strdup(str);
- new->len = len;
- new->next = (*head);
- (*head) = new;
+	return (NULL);
 
- return (*head);
+	new->str = strdup(str);
+	new->len = len;
+	new->next = (*head);
+	(*head) = new;
+
+	return (*head);
 }
 
